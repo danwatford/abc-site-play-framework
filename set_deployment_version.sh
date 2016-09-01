156f2dfad61478f69e4f68d7e01d0134c17daa4b
Daniel Watford <dan@foomoo.com>
2016-09-01 07:18:07 +0100

    Publish to bintray

---
+++ set_deployment_version.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Update bintray file descriptor with the build number as part of the version string.
+
+DESCRIPTOR="bintray-descriptor.json"
+
+sed -i "s/MICRO_VERSION/$TRAVIS_BUILD_NUMBER" "$DESCRIPTOR"
